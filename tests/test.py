ba20e5f22d5d8ff230c8618c66fe3d4c89e88101
lib_cgc <lib_cgc@gmail.com>
2018-04-30 06:56:38 -0700

    test.py

---
+++ tests/test.py
@@ -0,0 +1,8 @@
+import angr, angrop
+p = angr.Project("/bin/ls")
+rop = p.analyses.ROP()
+rop.find_gadgets()
+chain = rop.set_regs(rax=0x1337, rbx=0x56565656)
+chain.payload_str()
+#'\xb32@\x00\x00\x00\x00\x007\x13\x00\x00\x00\x00\x00\x00\xa1\x18@\x00\x00\x00\x00\x00VVVV\x00\x00\x00\x00'
+chain.print_payload_code()
