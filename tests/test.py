53aaa5261fd8f119d14ecdc0f1dda91fa4b6c33a
lib_cgc <lib_cgc@gmail.com>
2018-05-06 19:43:02 -0700

    not log

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 import angr, angrop
 import logging
-logging.getLogger("angrop").setLevel('DEBUG')
+#logging.getLogger("angrop").setLevel('DEBUG')
 p = angr.Project("/bin/ls")
 rop = p.analyses.ROP()
 rop.find_gadgets()
