698c2ae396aa2b4b2716c065324bc562fe2368b1
lib_cgc <lib_cgc@gmail.com>
2018-05-18 12:23:41 -0700

    find_func_r2.py find elf to analysis

--- tests/find_func_r2.py
+++ tests/find_func_r2.py
@@ -13,18 +13,22 @@ def get_file_name(file_dir):
                 file_elf.append(out_bytes.split(':')[0])
     return file_elf
 #return file name
-def get_file_name_strings(file_dir,string1):
+def get_file_name_strings(file_dir):
+    #system
+    string_interesting='"evil|system|read|recv|popen"'
     file_elf=[]
     for root,dirs,files in os.walk(file_dir):
         for file in files:
+            this_file=os.path.join(root,file)
             out_bytes=subprocess.check_output(['file',os.path.join(root,file)])
-            print out_bytes
+            #print 'file output:\n'+out_bytes
             if(out_bytes.find('ELF')!=-1):
                 try:
-                    out_bytes1=subprocess.check_output('strings '+os.path.join(root,file)+' |grep '+string1,shell=True)
-                    print out_bytes1
+                    out_bytes1=subprocess.check_output('strings '+os.path.join(root,file)+' |egrep '+string_interesting,shell=True)
+                    print 'string output: '+out_bytes1
                     if(out_bytes1!=''):
-                        file_elf.append(out_bytes.split(':')[0])
+                        print 'find file : '+this_file+' !!!!!!'
+                        file_elf.append(this_file)
                 except:
                     pass
     return file_elf
@@ -45,11 +49,12 @@ def get_func_elf(file_name_list,func_name):
 
 if __name__ == '__main__':
     #main()
-    if(len(sys.argv)!=3):
-        print "python find_func_r2.py dir string"
+    if(len(sys.argv)!=2):
+        print "python find_func_r2.py dir"
         exit()
-    dir= sys.argv[1]
-    string1 = sys.argv[2]
-    funcs=get_file_name_strings(dir,string1)
+    dir1= sys.argv[1]
+    #string1 = sys.argv[2]
+    files_name=get_file_name_strings(dir1)
+    
     #funcs1=get_func_elf(funcs,'parse_tiff_ifd')
     print 'ok'
